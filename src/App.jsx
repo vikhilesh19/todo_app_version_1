3777e1ed59c2ba71f30fc4d08ffac13b4002721a
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-06-28 14:06:43 +0530

    usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,22 @@
 import Appname from "./components/Appname"
 import Addtodo from "./components/Addtodo"
 import Items from "./components/Items";
-import { useState } from "react";
 import Welcomemsg from "./components/Welcomemsg";
 import { TodoItemsContext } from "./store/todo_items_store";
+import TOdoitemsContextprovider from "./store/todo_items_store";
+
 
 function App() {
-  let [todoitems,settodoitems]=useState([])
-
-  const handleadd=(itemname,itemdate)=>{
-    if(itemname && itemdate){
-      let newtodoitems=[...todoitems,
-      {
-        name:itemname,
-        date:itemdate,
-      }]
-      settodoitems(newtodoitems);
-    }
-    else{
-      alert('Enter Both Todo and date');
-    }
-  }
-
-  const handledelete=(itemname)=>{
-    let newtodoitems=todoitems.filter(item=>item.name!==itemname);
-    settodoitems(newtodoitems);
-  }
-
+  
   return (
-    <TodoItemsContext.Provider value={{
-      items:todoitems,
-      handleadd:handleadd,
-      handledelete:handledelete,
-    }}>
+    <TOdoitemsContextprovider>
       <center className='todo_container'>
         <Appname />
         <Addtodo />
         <Welcomemsg />
         <Items></Items>
       </center>
-    </TodoItemsContext.Provider>
+    </TOdoitemsContextprovider>
   )
 }
 export default App
--- src/store/todo_items_store.jsx
+++ src/store/todo_items_store.jsx
@@ -1,3 +1,59 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoItemsContext=createContext([]);
+
+const todoitemsreducer=(currenttodoitems,action)=>{
+  let newtodoitems=currenttodoitems;
+  if(action.type==='NEW_ITEM'){
+    newtodoitems=[...currenttodoitems,
+      {
+        name:action.payload.itemname,
+        date:action.payload.itemdate,
+      }]
+  }
+  else if(action.type==='DELETE_ITEM'){
+    newtodoitems=currenttodoitems.filter(item=>item.name!==action.payload.itemname);
+  }
+  return newtodoitems;
+}
+
+
+
+const TOdoitemsContextprovider=({children})=>{
+  // let [todoitems,settodoitems]=useState([]);
+  const [todoitems,dispatchtodoitems]=useReducer(todoitemsreducer,[]);
+
+  const handleadd=(itemname,itemdate)=>{
+    if(itemname && itemdate){
+      const newitemaction={
+        type:"NEW_ITEM",
+        payload:{
+          itemname,
+          itemdate,
+        }
+      };
+      dispatchtodoitems(newitemaction);
+    }
+    else{
+      alert('Enter Both Todo and date');
+    }
+  }
+
+  const handledelete=(itemname)=>{
+    const deleteitemaction={
+      type:"DELETE_ITEM",
+      payload:{
+        itemname,
+      }
+    };
+    dispatchtodoitems(deleteitemaction);
+  }
+  return(
+    <TodoItemsContext.Provider value={{
+      items:todoitems,
+      handleadd:handleadd,
+      handledelete:handledelete,
+    }}>{children}</TodoItemsContext.Provider>
+  )
+}
+export default TOdoitemsContextprovider;
